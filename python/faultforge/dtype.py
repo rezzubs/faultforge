8e580919e10b7677a170254873f43247ed427414
Marten Roots <marten.roots@gmail.com>
2026-06-12 15:51:39 +0300

    feat: Add public API for utility modules

---
+++ python/faultforge/dtype.py
@@ -0,0 +1,9 @@
+from faultforge._internal_new.dtype import (
+    EncodingDtype,
+    FiDtype,
+)
+
+__all__ = [
+    "EncodingDtype",
+    "FiDtype",
+]
---
+++ python/faultforge/tensor.py
@@ -0,0 +1,9 @@
+from faultforge._internal_new.tensor import (
+    tensor_list_dtype,
+    tensor_list_fault_injection,
+)
+
+__all__ = [
+    "tensor_list_dtype",
+    "tensor_list_fault_injection",
+]
