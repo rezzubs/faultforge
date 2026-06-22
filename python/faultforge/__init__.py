f927d580cca5303f5eeecaf532a9ab8ed58c8394
Marten Roots <marten.roots@gmail.com>
2026-06-22 15:20:25 +0300

    chore: Remove outdated package docs

--- python/faultforge/__init__.py
+++ python/faultforge/__init__.py
@@ -1,30 +1,4 @@
-"""Error correction and fault simulation for PyTorch.
-
-Systems
--------
-At the core of this library is the :class:`system.System` class, which
-represents a neural network model and its associated data. The system needs to
-expose a core data object and provide a method to get an accuracy metric based
-on that object. The system is also responsible for fault injection.
-
-The :class:`experiment.Experiment` class can be used to record experiments for a
-system over many runs of fault injection.
-
-System Implementations
-----------------------
-- The :mod:`imagenet` package provides a **system** implementation for ImageNet
-  models and related types.
-- The :mod:`cifar` package provides a **system** implementation for CIFAR models
-  and related types.
-- The :mod:`encoding` package provides different *encoded* **system** types
-  which wrap other **system** implementations.
-
-Utilities
----------
-The module :mod:`tensor_ops` provides utility functions for tensors and lists of
-tensors including fault injection and bitwise comparison.
-
-"""
+"""Error correction and fault simulation for PyTorch."""
 
 import sys
 
